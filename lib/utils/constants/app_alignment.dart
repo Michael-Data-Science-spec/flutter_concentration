e6396101a8c7512b38c1bccac8689b0fc408b48d
Michael-Data-Science-spec <76430924+Michael-Data-Science-spec@users.noreply.github.com>
2022-12-07 22:15:47 +0200

    Delete lib/utils/constants directory

--- lib/utils/constants/app_alignment.dart
+++
@@ -1,4 +0,0 @@
-class AppAlignment {
-  static const double toBottom = 65;
-  static const double interButton = 35;
-}
--- lib/utils/constants/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/animation.dart';
-
-class AppColors {
-  static const Color primaryBlue = Color(0xff7BC8F3);
-  static const Color textBlack = Color(0xff000000);
-  static const Color buttonPurple = Color(0xffC4B4F0);
-}
